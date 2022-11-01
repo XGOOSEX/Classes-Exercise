a51f424ba2e9d6422a9398bb8424866733485c92
Ian Simpson <ianbsimpson@gmail.com>
2022-11-01 13:29:11 -0700

    first commit

---
+++ Classes/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string makeInput, string modelInput, string yearInput)        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = yearInput;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }   
+        public string Year { get; set; }
+
+        
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Car() is a constructor - special member method. Helps us create a new instance of the Car Class
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = "2013";
+
+            var mazda = new Car()
+            {
+                Make = "Mazda", 
+                Model = "CX-5", 
+                Year = "2013"
+
+            };
+            var chevy = new Car("Chevy", "Impala", "2001");
+
+            var carList = new List<Car>() { myCar, mazda, chevy, };//Constructor
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+
+            }
+
+
         }
     }
 }
